b49f8de7da01bb7cfdc71428c41c91cb66e46f85
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-06-29 16:46:39 -0400

    Solidified generated HTML with its CSS

--- Source/ImageReadr.ts
+++ Source/ImageReadr.ts
@@ -7,7 +7,7 @@ module ImageReadr {
         /**
          * 
          */
-        private PixelRendr: PixelRendr.IPixelRendr;
+        private PixelRender: PixelRendr.IPixelRendr;
 
         /**
          * 
@@ -17,7 +17,7 @@ module ImageReadr {
         /**
          * 
          */
-        private palette: number[][];
+        private palette: string;
 
         /**
          * 
@@ -55,7 +55,9 @@ module ImageReadr {
             this.paletteDefault = settings.paletteDefault;
             this.palettes = settings.palettes;
 
-            this.palette = this.palettes[settings.paletteDefault];
+            this.palette = settings.paletteDefault;
+
+            this.initializePalettes();
         }
 
 
@@ -66,12 +68,64 @@ module ImageReadr {
          * 
          */
         private initializePalettes(): void {
-            var section: HTMLElement = document.getElementById(this.sectionSelector),
+            var section: HTMLElement = <HTMLElement>document.querySelector(this.sectionSelector),
                 name: string,
                 element: HTMLElement,
                 chosen: HTMLElement;
 
             section.appendChild(this.initializePaletteUploader());
+
+            for (name in this.palettes) {
+                if (!this.palettes.hasOwnProperty(name)) {
+                    continue;
+                }
+
+                element = this.initializePalette(name, this.palettes[name]);
+                section.appendChild(element);
+
+                if (name === this.paletteDefault) {
+                    chosen = element;
+                }
+            }
+
+            chosen.click();
+        }
+
+        /**
+         * 
+         */
+        private initializePalette(name, palette): HTMLDivElement {
+            var surround = document.createElement("div"),
+                label = document.createElement("h4"),
+                container = document.createElement("div"),
+                color, boxOut, boxIn, i;
+
+            surround.className = "palette";
+            label.className = "palette-label";
+            container.className = "palette-container";
+
+            surround.onclick = this.choosePalette.bind(this, surround, name, palette);
+
+            label.textContent = "Palette: " + name;
+
+            for (i = 0; i < palette.length; i += 1) {
+                color = palette[i];
+
+                boxOut = document.createElement("div");
+                boxOut.className = "palette-box";
+
+                boxIn = document.createElement("div");
+                boxIn.className = "palette-box-in";
+                boxIn.style.background = "rgba(" + color.join(",") + ")";
+
+                boxOut.appendChild(boxIn);
+                container.appendChild(boxOut);
+            }
+
+            surround.appendChild(label);
+            surround.appendChild(container);
+
+            return surround;
         }
 
         /**
@@ -94,6 +148,26 @@ module ImageReadr {
             surround.appendChild(label);
 
             return surround;
+        }
+
+        /**
+         * 
+         */
+        private choosePalette(element: HTMLElement, name, palette, event) {
+            var elements: HTMLCollection = element.parentElement.children,
+                i;
+
+            for (i = 0; i < elements.length; i += 1) {
+                (<HTMLElement>elements[i]).className = "palette"
+            }
+
+            element.className = "palette palette-selected";
+
+            this.PixelRender = new PixelRendr.PixelRendr({
+                "paletteDefault": palette
+            });
+
+            this.palette = name;
         }
     }
 }
